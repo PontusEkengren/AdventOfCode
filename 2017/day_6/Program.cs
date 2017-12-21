ae0603f252bc752e0319e55b639624e423ccab78
Pontus Carlsson <pontus.carlsson@comhem.com>
2017-12-21 13:38:27 +0100

    correct solve but .NET cant handle recursive

--- 2017/day_6/Program.cs
+++ 2017/day_6/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using day_6;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AdventOfCode
@@ -11,75 +13,11 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
+            Uberclass uc = new Uberclass();
+
             string puzzleInput = @"14	0	15	12	11	11	3	5	1	6	8	4	9	1	8	4";
-
-            FirstHalf(puzzleInput);
-            SecondHalf(puzzleInput);
+            uc.FirstHalf(puzzleInput);
+            uc.SecondHalf(puzzleInput);
         }
-
-
-        private static void FirstHalf(string puzzleInput)
-        {
-            Console.WriteLine("Puzzle 6 50%");
-
-            var valuesArray = puzzleInput.Split(' ', '\t').Select(Int32.Parse).ToList().ToArray();
-            Dictionary<int, int> values = new Dictionary<int, int>();
-
-            for (int i = 0; i < valuesArray.Length; i++)
-            {
-                values.Add(i, valuesArray[i]);
-            }
-
-            int maxvalue = values.Values.Max();
-            int maxvalue_index = values.First(x => x.Value == maxvalue).Key;
-
-
-            int numberOfRoundRobins = GoAllKalahaOnThatDictionary(new Dictionary<int, Dictionary<int, int>>(), values, maxvalue, maxvalue_index);
-
-            Console.WriteLine("checksum: " /*+ checksum*/);
-            Console.WriteLine();
-        }
-
-        private static int GoAllKalahaOnThatDictionary(Dictionary<int, Dictionary<int, int>> seenDictionarys, Dictionary<int, int> workingValues, int workingValue, int workingIndex)
-        {
-            int numberOfDicts = 0;
-
-            //workingDictionary = workingDictionary.Any() ? workingDictionary : initialValues;
-            seenDictionarys.Add(numberOfDicts++, workingValues);
-            Dictionary<int, int> newValues = new Dictionary<int, int>();
-
-            for (int i = 0; i < workingValue; i++)
-            {
-                workingValues[]++;
-                workingIndex = (workingIndex % workingValues.Keys.Max())
-                workingIndex++;
-            }
-
-            if (SeenThisDictBefore(seenDictionarys, newValues))
-            {
-                return numberOfDicts;
-            }
-            else
-            {
-                return GoAllKalahaOnThatDictionary(seenDictionarys, newValues);
-            }
-
-        } }
-
-        private static bool SeenThisDictBefore(Dictionary<int, Dictionary<int, int>> seenDictionarys, Dictionary<int, int> newValues)
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void SecondHalf(string puzzleInput)
-        {
-            Console.WriteLine("Puzzle 6 100%");
-
-
-
-            Console.WriteLine("checksum: " /*+ checksum*/);
-            Console.ReadKey();
-        }
-        
     }
 }
---
+++ 2017/day_6/Uberclass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_6
+{
+    public class Uberclass
+    {
+
+        public void FirstHalf(string puzzleInput)
+        {
+            Console.WriteLine("Puzzle 6 50%");
+
+            var valuesArray = puzzleInput.Split(' ', '\t').Select(Int32.Parse).ToList().ToArray();
+            Dictionary<int, int> values = new Dictionary<int, int>();
+
+            for (int i = 0; i < valuesArray.Length; i++)
+            {
+                values.Add(i, valuesArray[i]);
+            }
+
+            int numberOfRoundRobins = GoAllKalahaOnThatDictionary(new Dictionary<int, Dictionary<int, int>>(), values, values.Values.Max(), values.First(x => x.Value == values.Values.Max()).Key);
+
+            Console.WriteLine("checksum: " /*+ checksum*/);
+            Console.WriteLine();
+        }
+
+        private int GoAllKalahaOnThatDictionary(Dictionary<int, Dictionary<int, int>> seenDictionarys, Dictionary<int, int> workingValues, int workingValue, int workingIndex)
+        {
+            int NumberOfDictKey = seenDictionarys.Keys.Count > 0 ? seenDictionarys.Keys.Count + 1 : 0;
+            seenDictionarys.Add(NumberOfDictKey, workingValues);
+            workingValues[workingIndex] = 0;
+
+            while (workingValue > 0)
+            {
+                workingIndex++;
+                workingValues[((workingIndex) % workingValues.Keys.Count)]++;
+                workingValue--;
+            }
+
+            Dictionary<int, int> newValues = new Dictionary<int, int>(workingValues);
+            int maxvalue = newValues.Values.Max();
+            int maxvalue_index = newValues.First(x => x.Value == maxvalue).Key;
+
+            if (seenDictionarys.Any(x => x.Value == newValues))
+            {
+                return seenDictionarys.Keys.Count;
+            }
+            else
+            {
+                return GoAllKalahaOnThatDictionary(seenDictionarys, newValues, maxvalue, maxvalue_index);
+            }
+        }
+
+        public void SecondHalf(string puzzleInput)
+        {
+            Console.WriteLine("Puzzle 6 100%");
+
+
+
+            Console.WriteLine("checksum: " /*+ checksum*/);
+            Console.ReadKey();
+        }
+
+    }
+}
