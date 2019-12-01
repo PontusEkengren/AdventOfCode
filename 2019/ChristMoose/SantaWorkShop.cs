8fdebd8e43ba874d1215903d2a933f1fac32767f
Pontus Carlsson <pontus_carlsson@hotmail.com>
2019-12-01 14:13:27 +0100

    Day 1

---
+++ 2019/ChristMoose/SantaWorkShop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChristMoose
+{
+    class SantaWorkShop
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho Ho Ho!\n");
+
+            var day = new Day1();
+            //day.CalculateFuel_Part1();
+            day.CalculateFuel_Part2();
+
+            Console.ReadKey();
+        }
+    }
+}
